af8ef64259eda8bc9dbe975a74df6067c3168b54
Romain Norberg <romainnorberg@gmail.com>
2019-07-18 14:30:53 +0200

    add php-cs + github workflow

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of romainnorberg.be source code.
+ * (c) Romain Norberg <romainnorberg@gmail.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
