897592381766f817cbf201f09502ca9ae9d2aef1
Romain Norberg <romainnorberg@gmail.com>
2019-11-10 17:48:35 +0200

    add analytics tag

--- assets/js/admin/admin.js
+++ assets/js/admin/admin.js
@@ -3,17 +3,10 @@ require('../../css/admin/admin.scss');
 import EasyMDE from "easymde";
 import 'core-js';
 
-console.log('js');
-
-document.addEventListener('DOMContentLoaded', (ev) => {
-  console.log('js2');
-
+document.addEventListener('DOMContentLoaded', () => {
   Array.from(document.getElementsByClassName("markdown-editor")).forEach(
-    function(textarea, index, array) {
+    function (textarea, index, array) {
       new EasyMDE({element: textarea});
     }
   );
 })
-
-
-
