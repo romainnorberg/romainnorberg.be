a05c3bd385280937f6406f639ca70524473c9c82
Romain Norberg <romainnorberg@gmail.com>
2019-11-16 11:21:23 +0400

    fix ci build

--- tests/Domain/Blog/Controller/__snapshots__/BlogPostControllerTest__testBlogPost01__1.php
+++ tests/Domain/Blog/Controller/__snapshots__/BlogPostControllerTest__testBlogPost01__1.php
@@ -1,4 +1,5 @@
-<?php return '<p>We are using /SMS API services in this example</p>
+<?php return '
+<p>We are using /SMS API services in this example</p>
 <h3 id="prerequisites-assuming-that-you-have">Prerequisites, assuming that you have: <a href="#prerequisites-assuming-that-you-have" class="permalink">#</a>
 </h3>
 <ul>
@@ -29,7 +30,7 @@
 </h3>
 <p>Copy/paste content of <a href="https://gist.github.com/romainnorberg/9847a6707a26a77039358d85cce3d7aa#file-request-curl">request.curl</a> into prompt window <em>(File &gt; Import &gt; "Paste Raw text")</em> and confirm</p>
 <pre><code class="language-json hljs json" data-lang="json">curl -X GET \\
-  https://api.ovh.com/<span class="hljs-number">1.0</span>/sms \\
+ https://api.ovh.com/<span class="hljs-number">1.0</span>/sms \\
 -H \'Content-Type: application/json; charset=utf<span class="hljs-number">-8</span>\' \\
 -H \'X-Ovh-Application: xxxxxx\' \\
 -H \'X-Ovh-Consumer: {{lf_ovh_api_app_consumer_key}}\' \\
@@ -62,9 +63,9 @@
 <p>If you send the request as it is, the OVH API will return an error because the signature and the timestamp are not filled.</p>
 <p>Response after press "Send" button:</p>
 <pre><code class="language-json hljs json" data-lang="json">{
-  <span class="hljs-attr">"errorCode"</span>: <span class="hljs-string">"QUERY_TIME_OUT"</span>,
-  <span class="hljs-attr">"httpCode"</span>: <span class="hljs-string">"400 Bad Request"</span>,
-  <span class="hljs-attr">"message"</span>: <span class="hljs-string">"Query out of time"</span>
+ <span class="hljs-attr">"errorCode"</span>: <span class="hljs-string">"QUERY_TIME_OUT"</span>,
+ <span class="hljs-attr">"httpCode"</span>: <span class="hljs-string">"400 Bad Request"</span>,
+ <span class="hljs-attr">"message"</span>: <span class="hljs-string">"Query out of time"</span>
 }
 </code></pre>
 <h3 id="4-pre-request-script">4) Pre-request Script <a href="#4-pre-request-script" class="permalink">#</a>
@@ -72,42 +73,42 @@
 <p>Copy/paste content of <a href="https://gist.github.com/romainnorberg/9847a6707a26a77039358d85cce3d7aa#file-pre-request-script">pre-request-script</a> into "Pre-request Script" tab textarea.</p>
 <p><img src="https://thepracticaldev.s3.amazonaws.com/i/xvyl6sf6qboq6gy9xpfy.png" alt="pre-request script tab"></p>
 <pre><code class="language-javascript hljs javascript" data-lang="javascript">pm.sendRequest({
-    <span class="hljs-attr">url</span>: <span class="hljs-string">"https://api.ovh.com/1.0/auth/time"</span>,
-    <span class="hljs-attr">method</span>: <span class="hljs-string">"GET"</span>,
-    <span class="hljs-attr">headers</span>: {
-        <span class="hljs-string">\'Content-Type\'</span>: <span class="hljs-string">\'application/json; charset=utf-8\'</span>
-    },
-    <span class="hljs-attr">body</span>: {}
+ <span class="hljs-attr">url</span>: <span class="hljs-string">"https://api.ovh.com/1.0/auth/time"</span>,
+ <span class="hljs-attr">method</span>: <span class="hljs-string">"GET"</span>,
+ <span class="hljs-attr">headers</span>: {
+ <span class="hljs-string">\'Content-Type\'</span>: <span class="hljs-string">\'application/json; charset=utf-8\'</span>
+ },
+ <span class="hljs-attr">body</span>: {}
  },
  <span class="hljs-function"><span class="hljs-keyword">function</span> (<span class="hljs-params">err, res</span>) </span>{
-     
-    pm.expect(err).to.not.be.ok;
-    pm.expect(res).to.have.property(<span class="hljs-string">\'code\'</span>, <span class="hljs-number">200</span>);
-    pm.expect(res).to.have.property(<span class="hljs-string">\'status\'</span>, <span class="hljs-string">\'OK\'</span>);
-    
-    <span class="hljs-keyword">var</span> serverTimestamp = res.text();
-    postman.setGlobalVariable(<span class="hljs-string">"lf_ovh_api_app_server_timestamp"</span>, serverTimestamp);
-    
-    <span class="hljs-keyword">var</span> time_delta = serverTimestamp - <span class="hljs-built_in">Math</span>.round(<span class="hljs-keyword">new</span> <span class="hljs-built_in">Date</span>().getTime()/<span class="hljs-number">1000</span>);
-    <span class="hljs-keyword">var</span> now = <span class="hljs-built_in">Math</span>.round(<span class="hljs-keyword">new</span> <span class="hljs-built_in">Date</span>().getTime()/<span class="hljs-number">1000</span>) + time_delta;
-    <span class="hljs-keyword">var</span> body = <span class="hljs-string">\'\'</span>;
-    
-    postman.setGlobalVariable(<span class="hljs-string">"lf_ovh_api_app_timestamp"</span>, now);
-    
-    <span class="hljs-keyword">var</span> toSign = environment.lf_ovh_api_app_secret + <span class="hljs-string">\'+\'</span> + environment.lf_ovh_api_app_consumer_key + <span class="hljs-string">\'+\'</span> + pm.request.method + <span class="hljs-string">\'+\'</span> + pm.request.url.toString() + <span class="hljs-string">\'+\'</span> + body + <span class="hljs-string">\'+\'</span> + now;
-    
-    <span class="hljs-keyword">var</span> signature = <span class="hljs-string">\'$1$\'</span> + CryptoJS.SHA1(toSign);
-    
-    postman.setGlobalVariable(<span class="hljs-string">"lf_ovh_api_app_consumer_signature"</span>, signature);
+ 
+ pm.expect(err).to.not.be.ok;
+ pm.expect(res).to.have.property(<span class="hljs-string">\'code\'</span>, <span class="hljs-number">200</span>);
+ pm.expect(res).to.have.property(<span class="hljs-string">\'status\'</span>, <span class="hljs-string">\'OK\'</span>);
+ 
+ <span class="hljs-keyword">var</span> serverTimestamp = res.text();
+ postman.setGlobalVariable(<span class="hljs-string">"lf_ovh_api_app_server_timestamp"</span>, serverTimestamp);
+ 
+ <span class="hljs-keyword">var</span> time_delta = serverTimestamp - <span class="hljs-built_in">Math</span>.round(<span class="hljs-keyword">new</span> <span class="hljs-built_in">Date</span>().getTime()/<span class="hljs-number">1000</span>);
+ <span class="hljs-keyword">var</span> now = <span class="hljs-built_in">Math</span>.round(<span class="hljs-keyword">new</span> <span class="hljs-built_in">Date</span>().getTime()/<span class="hljs-number">1000</span>) + time_delta;
+ <span class="hljs-keyword">var</span> body = <span class="hljs-string">\'\'</span>;
+ 
+ postman.setGlobalVariable(<span class="hljs-string">"lf_ovh_api_app_timestamp"</span>, now);
+ 
+ <span class="hljs-keyword">var</span> toSign = environment.lf_ovh_api_app_secret + <span class="hljs-string">\'+\'</span> + environment.lf_ovh_api_app_consumer_key + <span class="hljs-string">\'+\'</span> + pm.request.method + <span class="hljs-string">\'+\'</span> + pm.request.url.toString() + <span class="hljs-string">\'+\'</span> + body + <span class="hljs-string">\'+\'</span> + now;
+ 
+ <span class="hljs-keyword">var</span> signature = <span class="hljs-string">\'$1$\'</span> + CryptoJS.SHA1(toSign);
+ 
+ postman.setGlobalVariable(<span class="hljs-string">"lf_ovh_api_app_consumer_signature"</span>, signature);
  }
 );
 </code></pre>
 <h3 id="5-try-to-run">5) Try to run <a href="#5-try-to-run" class="permalink">#</a>
 </h3>
 <p>Normally you should then have the following response from OVH API 🎉🎉</p>
-<pre><code class="language-json hljs json" data-lang="json">  [
-    <span class="hljs-string">"sms-rnXXXXX-1"</span>
-  ]
+<pre><code class="language-json hljs json" data-lang="json"> [
+ <span class="hljs-string">"sms-rnXXXXX-1"</span>
+ ]
 </code></pre>
 <p>😠the API returns you an error? check your credentials, check "Possibles issues" section below.</p>
 <h2 id="possibles-issues">Possibles issues <a href="#possibles-issues" class="permalink">#</a>
@@ -135,4 +136,5 @@
 <p><img src="https://docs.ovh.com/fr/private-cloud/connexion-a-l-api-ovh/images/api.jpg" alt="ovh-api-service"></p>
 <h2 id="script-sources">Script sources <a href="#script-sources" class="permalink">#</a>
 </h2>
-<p>Check <a href="https://gist.github.com/romainnorberg/9847a6707a26a77039358d85cce3d7aa">https://gist.github.com/romainnorberg/9847a6707a26a77039358d85cce3d7aa</a></p>';
+<p>Check <a href="https://gist.github.com/romainnorberg/9847a6707a26a77039358d85cce3d7aa">https://gist.github.com/romainnorberg/9847a6707a26a77039358d85cce3d7aa</a></p>
+ ';
