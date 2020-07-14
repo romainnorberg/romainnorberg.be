9c9562a6262ca115255d7303bbeeebe6f54d9bcb
Romain Norberg <7681951+romainnorberg@users.noreply.github.com>
2020-07-14 06:40:05 +0200

    Use PEST (#33)
    * Use PEST
    
    * update packages to fix snapshots location
    
    * update to 0.2

--- tests/Domain/Blog/Controller/BlogPostControllerTest.php
+++ tests/Domain/Blog/Controller/BlogPostControllerTest.php
@@ -1,31 +1,13 @@
 <?php
 
-/*
- * This file is part of romainnorberg.be source code.
- * (c) Romain Norberg <romainnorberg@gmail.com>
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+test('blog_post_markdown_syntax', function () {
+    $client = static::createClient();
+    $client->setServerParameter('HTTP_HOST', getenv('BLOG_DOMAIN'));
 
-namespace App\Tests\Domain\Blog\Controller;
+    $crawler = $client->request('GET', '/post/work-with-ovh-api-using-postman');
 
-use Spatie\Snapshots\MatchesSnapshots;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+    $this->assertResponseIsSuccessful();
+    $this->assertSelectorTextContains('title', 'Work with OVH API using Postman');
 
-class BlogPostControllerTest extends WebTestCase
-{
-    use MatchesSnapshots;
-
-    public function testBlogPost01()
-    {
-        $client = static::createClient();
-        $client->setServerParameter('HTTP_HOST', getenv('BLOG_DOMAIN'));
-
-        $crawler = $client->request('GET', '/post/work-with-ovh-api-using-postman');
-
-        $this->assertSame(200, $client->getResponse()->getStatusCode(), $crawler->filter('h1.exception-message')->count() > 0 ? $crawler->filter('h1.exception-message')->html() : 'Unknown');
-        $this->assertStringContainsString('Work with OVH API using Postman', $crawler->filter('title')->html());
-
-        $this->assertMatchesSnapshot(preg_replace('/\s*/m', '', preg_replace("/[\r\n]+/", "", $crawler->filter('div.markup')->html())));
-    }
-}
+    $this->assertMatchesSnapshot(preg_replace('/\s*/m', '', preg_replace("/[\r\n]+/", "", $crawler->filter('div.markup')->html()))); // avoid CI failure with blank return
+});
